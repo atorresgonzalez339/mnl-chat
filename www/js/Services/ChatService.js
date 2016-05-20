d83537e4c91d926a9bfacfba07b6568440927b0d
Angel Torres <atorres@reloquest.com>
2016-05-20 18:42:36 -0400

    Add Chat functionality

--- www/js/Services/ChatService.js
+++ www/js/Services/ChatService.js
@@ -17,20 +17,59 @@ chat_services.factory('ChatService', function ($q, FireManager) {
     FIRE_CHAT.on('room-invite', function (data) {
         console.log('New Conversation Invite Recibed');
         console.log(data);
+        FIRE_CHAT.acceptInvite(data.id, function(){
+            console.log('Invitation Accepted');
+            //Do anything
+        })
     });
 
     return {
         createConversation: function (user_id) {
-            FIRE_CHAT.createRoom(roomName, 'private', function (roomId) {
-                FIRE_CHAT.inviteUser(user_id, roomId);
-                FIRE_CHAT.enterRoom(roomId);
-                Console.log('Conversation started');
-            })
+            return $q(function (resolve, reject) {
+                var conversationId = 'conversation1';
+                FIRE_CHAT.createRoom(conversationId, 'private', function (roomId) {
+                    FIRE_CHAT.inviteUser(user_id, roomId);
+                    FIRE_CHAT.enterRoom(roomId);
+                    resolve();
+                })
+            });
+        },
+        hasConversationOpen: function(user_id){
+            return $q(function (resolve, reject) {
+                FIRE_CHAT.getRoomList(function (allRooms) {
+                    var roomKeys = Object.keys(allRooms);
+                    var hasRoom = false;
+                    for(var i = 0; i < roomKeys.length; i++ ){
+                        var room = allRooms[roomKeys[i]];
+                        if(Object.has(room.authorizedUsers, user_id)){
+                            hasRoom = room;
+                            break;
+                        }
+                    }
+                    resolve(hasRoom);
+                });
+            });
         },
         getConversationList: function () {
             return $q(function (resolve, reject) {
                 FIRE_CHAT.getRoomList(function (data) {
-                    console.log(data);
+                    resolve(data);
+                });
+            });
+        },
+        getConversation: function (roomId) {
+            return $q(function (resolve, reject) {
+                FIRE_CHAT.getRoom(roomId, function(data){
+                    resolve(data);
+                });
+            });
+        },
+        getConversationUserId: function (roomId) {
+            return $q(function (resolve, reject) {
+                var distinct_id = FireManager.getFireAuth().$getAuth().uid;
+                FIRE_CHAT.getRoom(roomId, function(room){
+                    var authorizedIds = Object.keys(room.authorizedUsers).exclude(distinct_id);
+                    resolve(authorizedIds[0]);
                 });
             });
         }
